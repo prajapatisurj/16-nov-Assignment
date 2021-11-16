d0c906fbce7d81eeba5eff871d55e00a14a32fd3
prajapatisurj <47967303+prajapatisurj@users.noreply.github.com>
2021-11-16 12:19:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, CssBaseline } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import theme from "./theme";
+
+import Students from "./components/students/Students";
+import Student from "./components/students/Student";
+import AddStudent from "./components/students/AddStudent";
+import EditStudent from "./components/students/EditStudent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Container>
+          <Switch>
+            <Route exact path="/" component={Students} />
+            <Route exact path="/students/create" component={AddStudent} />
+            <Route exact path="/students/:id" component={Student} />
+            <Route exact path="/students/:id/edit" component={EditStudent} />
+          </Switch>
+        </Container>
+      </ThemeProvider>
+    </Router>
   );
 }
 
